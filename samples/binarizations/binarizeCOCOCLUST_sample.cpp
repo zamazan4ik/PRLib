af7495fe384ebd69b3091715946b1a2e4bd09fd1
Alexander Zaitsev <zamazan4ik@gmail.com>
2017-09-27 17:33:26 +0300

    Added binarizations samples

---
+++ samples/binarizations/binarizeCOCOCLUST_sample.cpp
@@ -0,0 +1,28 @@
+#include "binarizeCOCOCLUST.h"
+
+#include <stdexcept>
+#include <string>
+
+int main(int argc, char**argv)
+{
+    const std::string inputImageFilename = argv[1];
+    const std::string outputImageFilename = argv[2];
+
+    if (inputImageFilename.empty())
+    {
+        throw std::invalid_argument("Input image filename is empty.");
+    }
+
+    if (outputImageFilename.empty())
+    {
+        throw std::invalid_argument("Output image file name is empty.");
+    }
+
+    cv::Mat inputImage = cv::imread(inputImageFilename);
+    cv::Mat outputImage;
+
+    prl::binarizeCOCOCLUST(inputImage, outputImage);
+
+    cv::imwrite(outputImageFilename, outputImage);
+}
+
---
+++ samples/binarizations/binarizeSauvola_sample.cpp
@@ -0,0 +1,27 @@
+#include "binarizeSauvola.h"
+
+#include <stdexcept>
+#include <string>
+
+int main(int argc, char**argv)
+{
+    const std::string inputImageFilename = argv[1];
+    const std::string outputImageFilename = argv[2];
+
+    if (inputImageFilename.empty())
+    {
+        throw std::invalid_argument("Input image filename is empty.");
+    }
+
+    if (outputImageFilename.empty())
+    {
+        throw std::invalid_argument("Output image file name is empty.");
+    }
+
+    cv::Mat inputImage = cv::imread(inputImageFilename);
+    cv::Mat outputImage;
+
+    prl::binarizeSauvola(inputImage, outputImage);
+
+    cv::imwrite(outputImageFilename, outputImage);
+}
