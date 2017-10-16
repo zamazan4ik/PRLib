c43d6d2d4a590fc2e8723edc778e7f5269e484d9
Alexander Zaitsev <zamazan4ik@tut.by>
2017-10-17 00:57:30 +0300

    Add SaltPepper denoise

---
+++ src/denoise/denoiseSaltPepper.cpp
@@ -0,0 +1,12 @@
+#include "denoiseSaltPepper.h"
+
+#include <opencv2/imgproc/imgproc.hpp>
+
+void prl::denoiseSaltPepper(const cv::Mat& inputImage, cv::Mat outputImage, int kernelSize, int times)
+{
+    outputImage = inputImage.clone();
+    for(size_t i = 0; i < times; ++i)
+    {
+        cv::medianBlur(outputImage, outputImage, kernelSize);
+    }
+}
---
+++ src/denoise/denoiseSaltPepper.h
@@ -0,0 +1,11 @@
+#ifndef PRLIB_DENOISESALTPEPPER_H
+#define PRLIB_DENOISESALTPEPPER_H
+
+#include <opencv2/core/core.hpp>
+
+namespace prl
+{
+void denoiseSaltPepper(const cv::Mat& inputImage, cv::Mat outputImage, int kernelSize, int times);
+}
+
+#endif //PRLIB_DENOISESALTPEPPER_H
