eb3e49f3c622592d7fbed4fba83bffbf60ff11a3
Alexander Zaitsev <zamazan4ik@tut.by>
2017-10-17 10:28:33 +0300

    Add denoiseSaltPepper sample

--- samples/denoise/denoiseSaltPepper_sample.cpp
+++ samples/denoise/denoiseSaltPepper_sample.cpp
@@ -1,4 +1,31 @@
-//
-// Created by zamazan4ik on 17.10.17.
-//
+#include "denoiseSaltPepper.h"
+
+#include <opencv2/core/core.hpp>
+#include <opencv2/highgui/highgui.hpp>
+
+#include <stdexcept>
+#include <string>
+
+int main(int argc, char**argv)
+{
+    const std::string inputImageFilename = argv[1];
+    const std::string outputImageFilename = argv[2];
+
+    if (inputImageFilename.empty())
+    {
+        throw std::invalid_argument("Input image filename is empty.");
+    }
+
+    if (outputImageFilename.empty())
+    {
+        throw std::invalid_argument("Output image file name is empty.");
+    }
+
+    cv::Mat inputImage = cv::imread(inputImageFilename);
+    cv::Mat outputImage;
+
+    prl::denoiseSaltPepper(inputImage, outputImage, 3, 1);
+
+    cv::imwrite(outputImageFilename, outputImage);
+}
 
