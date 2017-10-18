4724a2dd2454c8cb26adacf83389fa30a15c2bdb
Alexander Zaitsev <zamazan4ik@tut.by>
2017-10-18 21:20:37 +0300

    Add autocrop sample

--- samples/border_detection/autoCrop_sample.cpp
+++ samples/border_detection/autoCrop_sample.cpp
@@ -1,4 +1,31 @@
-//
-// Created by zamazan4ik on 18.10.17.
-//
+#include "autoCrop.h"
+
+#include <opencv2/core/core.hpp>
+#include <opencv2/highgui/highgui.hpp>
+
+#include <stdexcept>
+#include <string>
+
+int main(int argc, char**argv)
+{
+    const std::string inputImageFilename = argv[1];
+    const std::string outputImageFilename = argv[2];
+
+    if (inputImageFilename.empty())
+    {
+        throw std::invalid_argument("Input image filename is empty.");
+    }
+
+    if (outputImageFilename.empty())
+    {
+        throw std::invalid_argument("Output image file name is empty.");
+    }
+
+    cv::Mat inputImage = cv::imread(inputImageFilename);
+    cv::Mat outputImage;
+
+    prl::autoCrop(inputImage, outputImage);
+
+    cv::imwrite(outputImageFilename, outputImage);
+}
 
