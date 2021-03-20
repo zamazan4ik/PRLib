eac420c78f770a92fed789527a3e0e98612d9dac
Alexander Zaitsev <zamazan4ik@tut.by>
2021-03-20 15:15:10 +0300

    refactor: cleanup several places
    - cleanup old cmake code
    - fix warn in splitPage algorithm
    - add build directory to gitignore
    
    Tested:
    - Local build

--- src/splitPage.cpp
+++ src/splitPage.cpp
@@ -32,6 +32,7 @@
 #include <opencv2/core/core.hpp>
 #include <opencv2/imgproc/imgproc.hpp>
 
+// PLEASE DON'T USE IT SINCE IT'S WIP!
 std::pair<cv::Point, cv::Point> findVertLine(const cv::Mat &inputImage) {
 
     cv::Mat imageToProc = inputImage.clone();
@@ -71,7 +72,8 @@ std::pair<cv::Point, cv::Point> findVertLine(const cv::Mat &inputImage) {
     int center = resultCanny.cols / 2;
     for(const auto& line : vertLines)
     {
-        auto lineByPoint = fromVec2f(line);
-        
+        auto lineByPoint = fromVec2f(line); 
     }
+
+    return std::make_pair(cv::Point(0, 0), cv::Point(0, 0));
 };
